c9120bda34c8411458d5053f0f4115b0906bfbc5
itosho <sho.ito@connehito.com>
2020-05-03 23:31:00 +0900

    Add counter

--- src/App.js
+++ src/App.js
@@ -1,32 +1,31 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React, { Component } from 'react'
 
-const App= () => {
-  const profiles = [
-    {name: "Taro", age: 10},
-    {name: "Hanako", age: 8},
-    {name: "Test", age: 100}
-  ]
-  return (
-    <React.Fragment>
-      {
-      profiles.map((profile, index) => {
-        return <User key={index} name={profile.name} age={profile.age} />
-      })
-      }
-    </React.Fragment>
-  )
-}
+const App = () => (<Counter />)
 
-const User = (props) => {
-  return (
-    <p>Hi, I am {props.name} and {props.age} years old!</p>
-  )
-}
+class Counter extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { count: 0 }
+  }
 
-User.propTypes = {
-  name: PropTypes.string,
-  age: PropTypes.number.isRequired
+  handlePlusButton = () => {
+    this.setState({count: this.state.count + 1})
+  }
+
+  handleMinusButton = () => {
+    this.setState({count: this.state.count - 1})
+  }
+
+  render() {
+    console.log("render")
+    return (
+      <React.Fragment>
+        <div>count: {this.state.count}</div>
+        <button onClick={this.handlePlusButton}>+1</button>
+        <button onClick={this.handleMinusButton}>-1</button>
+      </React.Fragment>
+    )
+  }
 }
 
 export default App
