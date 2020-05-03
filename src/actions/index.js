e0ed262e98877857d3937ea5eb3adf9218d1d550
itosho <sho.ito@connehito.com>
2020-05-04 00:06:06 +0900

    Add action creator

---
+++ src/actions/index.js
@@ -0,0 +1,10 @@
+export const INCREMENT = 'INCREMENT'
+export const DECREMRNT = 'DECREMENT'
+
+export const increment = () => ({
+    type: INCREMENT
+})
+
+export const decrement = () => ({
+    type: DECREMRNT
+})
