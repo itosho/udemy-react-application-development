b0a43807705938e34c556e6a66b1d8b589aab5e1
itosho <sho.ito@connehito.com>
2020-05-05 03:58:18 +0900

    Add prettierrc

--- src/components/events_index.js
+++ src/components/events_index.js
@@ -1,24 +1,23 @@
 import React from 'react'
 import { connect } from 'react-redux'
-
 import { increment, decrement } from '../actions'
 
 const EventsIndex = (props) => {
-    return (
-      <React.Fragment>
-        <div>count value: {props.value}</div>
-        <button onClick={props.increment}>+1</button>
-        <button onClick={props.decrement}>-1</button>
-      </React.Fragment>
-    )
+  return (
+    <React.Fragment>
+      <div>count value: {props.value}</div>
+      <button onClick={props.increment}>+1</button>
+      <button onClick={props.decrement}>-1</button>
+    </React.Fragment>
+  )
 }
 
-const mapStateToProps = state => ({ value: state.count.value })
+const mapStateToProps = (state) => ({ value: state.count.value })
 // const mapDispatchToProps = dispatch => ({
 //   increment: () => dispatch(increment()),
 //   decrement: () => dispatch(decrement())
 // })
 // shorthand
-const mapDispatchToProps = ({ increment, decrement })
+const mapDispatchToProps = { increment, decrement }
 
 export default connect(mapStateToProps, mapDispatchToProps)(EventsIndex)
