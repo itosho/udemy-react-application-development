1f2771dfd3068971d3ba88dd3707428734adb66e
itosho <sho.ito@connehito.com>
2020-05-10 18:24:57 +0900

    Add update action

--- src/components/events_show.js
+++ src/components/events_show.js
@@ -40,7 +40,7 @@ class EventsShow extends Component {
   }
 
   async onSubmit(values) {
-    // await this.props.postEvent(values)
+    await this.props.putEvent(values)
     this.props.history.push('/')
   }
 
@@ -71,7 +71,7 @@ const mapStateToProps = (state, ownProps) => {
   const event = state.events[ownProps.match.params.id]
   return { initialValues: event, event }
 }
-const mapDispatchToProps = { deleteEvent, getEvent }
+const mapDispatchToProps = { deleteEvent, getEvent, putEvent }
 
 const validate = (values) => {
   const errors = {}
--- src/reducers/events.js
+++ src/reducers/events.js
@@ -1,11 +1,12 @@
 import _ from 'lodash'
-import { READ_EVENTS, READ_EVENT, DELETE_EVENT } from '../actions'
+import { READ_EVENTS, READ_EVENT, UPDATE_EVENT, DELETE_EVENT } from '../actions'
 
 export default (events = {}, action) => {
   switch (action.type) {
     case READ_EVENTS:
       return _.mapKeys(action.response.data, 'id')
     case READ_EVENT:
+    case UPDATE_EVENT:
       const data = action.response.data
       return { ...events, [data.id]: data }
     case DELETE_EVENT:
